0cfe68d714ce7a20a5cc7cc231b118c9695c1ce9
PinteaGeorge <57567259+PinteaGeorge@users.noreply.github.com>
2020-04-08 10:54:13 +0300

    Add files via upload

---
+++ Playing-Card/Playing-Card/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Playing_Card
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PlayingCard p1 = new PlayingCard(4, 13);
+            Console.WriteLine(p1+  p1.SuitToString() +", "+p1.RankToString());
+            Console.ReadKey();
+        }
+       
+    }
+}
