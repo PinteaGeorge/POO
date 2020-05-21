842d4e87654b1667055130d06afbd96a7e8c4eb9
PinteaGeorge <57567259+PinteaGeorge@users.noreply.github.com>
2020-05-21 10:38:42 +0300

    Add files via upload

---
+++ BigNumber/BigNumber/BigNumber.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BigNumber
+{
+    public class BigNumber
+    {
+        public List<int> digits;
+        public BigNumber()
+        {
+            digits = new List<int>();
+        }
+        public BigNumber(params int[] digits)
+        {
+            this.digits = new List<int>();
+            for (int i = 0; i < digits.Length; i++)
+            {
+                if (digits[i] > 9)
+                {
+                    List<int> ndig = new List<int>();
+                    while (digits[i] > 0)
+                    {
+                        ndig.Add(digits[i] % 10);
+                        digits[i] /= 10;
+                    }
+                    for (int d = ndig.Count - 1; d >= 0; d--)
+                    {
+                        this.digits.Add(ndig[d]);
+                    }
+                }
+                else
+                {
+                    this.digits.Add(digits[i]);
+                }
+            }
+        }
+
+        public BigNumber Clone()
+        {
+            BigNumber clone = new BigNumber();
+            for (int i = 0; i < digits.Count; i++)
+                clone.digits.Add(digits[i]);
+            return clone;
+        }
+
+        public BigNumber Invert()
+        {
+            BigNumber bgi = new BigNumber();
+            for (int i = digits.Count - 1; i >= 0; i--)
+                bgi.digits.Add(digits[i]);
+            return bgi;
+        }
+
+        public static BigNumber operator +(BigNumber bn1, BigNumber bn2)
+        {
+            BigNumber sum = new BigNumber();
+            int maxLen = bn1.digits.Count > bn2.digits.Count ? bn1.digits.Count : bn2.digits.Count;
+            int carry = 0;
+            int i = 0;
+            while (i < maxLen)
+            {
+                int a = i < bn1.digits.Count ? bn1.digits[bn1.digits.Count - i - 1] : 0;
+                int b = i < bn2.digits.Count ? bn2.digits[bn2.digits.Count - i - 1] : 0;
+                sum.digits.Add((a + b + carry) % 10);
+                carry = (a + b + carry) / 10;
+                i++;
+            }
+            if (carry > 0)
+                sum.digits.Add(carry);
+            return sum.Invert();
+        }
+
+        public static BigNumber operator *(BigNumber bn1, BigNumber bn2)
+        {
+            BigNumber prod = new BigNumber();
+            for (int i = 0; i < bn1.digits.Count + bn2.digits.Count; i++)
+                prod.digits.Add(0);
+            int k = prod.digits.Count - 1;
+            int shift = 0;
+            for (int i = bn1.digits.Count - 1; i >= 0; i--)
+            {
+                int a = bn1.digits[i];
+                for (int j = bn2.digits.Count - 1; j >= 0; j--)
+                {
+                    int b = bn2.digits[j];
+                    prod.digits[k - shift] += a * b;
+                    prod.digits[k - shift - 1] += prod.digits[k - shift] / 10;
+                    prod.digits[k - shift] %= 10;
+                    k--;
+                }
+                shift++;
+                k = prod.digits.Count - 1;
+            }
+            while (prod.digits[0] == 0)
+                prod.digits.RemoveAt(0);
+            return prod;
+        }
+
+        public override string ToString()
+        {
+            string result = "";
+            for (int i = 0; i < digits.Count; i++)
+                result += digits[i];
+            return result;
+        }
+    }
+}
---
+++ BigNumber/BigNumber/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BigNumber
+{
+    class Program
+    {
+        public static BigNumber Fibonacci(int n)
+        {
+            BigNumber a = new BigNumber(0);
+            BigNumber b = new BigNumber(1);
+            int nr = 2;
+            if (n <= 1) return a;
+            if (n == 2) return b;
+            while (nr < n)
+            {
+                nr++;
+                BigNumber c = a + b;
+                a = b.Clone();
+                b = c.Clone();
+            }
+            return b;
+        }
+
+        public static BigNumber Factorial(int n)
+        {
+            BigNumber result = new BigNumber(1);
+            while (n > 0)
+            {
+                result *= new BigNumber(n);
+                n--;
+            }
+            return result;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("1000th number of fibonacci series: " + Fibonacci(1000));
+            Console.WriteLine();
+            Console.WriteLine("factorial of 1000: " + Factorial(1000));
+            Console.ReadLine();
+        }
+    }
+}
