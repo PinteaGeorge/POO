73d0ac2311322d81be1682ba7a902d93bffe56d5
PinteaGeorge <57567259+PinteaGeorge@users.noreply.github.com>
2020-05-21 10:50:03 +0300

    Add files via upload

---
+++ SortareLocalizata/SortareLocalizata/Person.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortareLocalizata
+{
+    public class Person
+    {
+        public string LastName;
+        public string FirstName;
+        public string SecondName;
+        public Person()
+        {
+            LastName = null;
+            FirstName = null;
+            SecondName = null;
+        }
+        public void CorrectFormat()
+        {
+            if (LastName != null)
+            {
+                LastName = LastName.ToLower();
+                string name = (char)(LastName[0] - 32) + LastName.Substring(1);
+                LastName = name;
+            }
+            if (FirstName != null)
+            {
+                FirstName = FirstName.ToLower();
+                string name = (char)(FirstName[0] - 32) + FirstName.Substring(1);
+                FirstName = name;
+            }
+            if (SecondName != null)
+            {
+                SecondName = SecondName.ToLower();
+                string name = (char)(SecondName[0] - 32) + SecondName.Substring(1);
+                SecondName = name;
+            }
+        }
+    }
+}
---
+++ SortareLocalizata/SortareLocalizata/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortareLocalizata
+{
+    class Program
+    {
+        public static bool CheckDivider(char c, char[] dividers)
+        {
+            foreach (char divider in dividers)
+                if (c == divider) 
+                    return true;
+            return false;
+        }
+        static void Main(string[] args)
+        {
+            List<Person> people = new List<Person>();
+            char[] dividers = { ' ', ',', '.', ';', '/', '\\', '-', '_' };
+            string[] lines = File.ReadAllLines("../../input.txt");
+            foreach (string line in lines)
+            {
+                Person p = new Person();
+                string name = "";
+                bool onWord = false;
+                for (int i = 0; i < line.Length; i++) 
+                {
+                    char c = line[i];
+                    if (onWord)
+                    {
+                        if (CheckDivider(c, dividers) || i == line.Length - 1)
+                        {
+                            if (i == line.Length - 1)
+                                name += c;
+                            if (p.LastName == null)
+                                p.LastName = name;
+                            else if (p.FirstName == null)
+                                p.FirstName = name;
+                            else if (p.SecondName == null)
+                                p.SecondName = name;
+                            name = "";
+                            onWord = false;
+                        }
+                        else
+                        {
+                            name += c;
+                        }
+                    }
+                    else
+                    {
+                        if (!CheckDivider(c, dividers))
+                        {
+                            onWord = true;
+                            name += c;
+                        }
+                    }
+                }
+                p.CorrectFormat();
+                people.Add(p);
+            }
+            people = people.OrderBy(p => p.LastName).ThenBy(p => p.FirstName).ThenBy(p => p.SecondName).ToList();
+            string output = "";
+            foreach (Person person in people)
+            {
+                output += person.LastName + " " + person.FirstName + " " + person.SecondName + "\n";
+            }
+            File.WriteAllText("../../output.txt", output);
+            Console.ReadLine();
+        }
+    }
+}
